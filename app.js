064480bcc51928f9ff461f195c891dcb2fdeee37
Olga <olganum@gmail.com>
2022-08-06 02:07:26 +0300

    проба

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authorization = require('./middlewares/authorization');
 const pageNotFound = require('./middlewares/pageNotFound');
+const { login } = require('./controllers/users');
 
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
@@ -22,5 +23,7 @@ app.listen(PORT);
 
 app.use('/users', require('./routes/users')); // запускаем, при запросе на '/users' срабатывает роутер './routes/users'
 app.use('/cards', require('./routes/cards')); // запускаем, при запросе на '/cards' срабатывает роутер './routes/cards'
+
+app.post('/signin', login);
 
 app.use(pageNotFound);
