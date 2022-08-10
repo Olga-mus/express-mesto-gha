f16fc51091b587a2ff8afde3e75f67038cbe45e6
Olga <olganum@gmail.com>
2022-08-10 22:23:19 +0300

    отправка работы

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken'); // импортируем модуль jsonwebtoken
 
 const SECRET_KEY = 'secret_key';
-// payload - это то, что хотим зашифровать
+// payload -это то, что хотим зашифровать
 // eslint-disable-next-line arrow-body-style
 const generateToken = (payload) => {
   return jwt.sign(payload, SECRET_KEY, { expiresIn: '7d' });// что хотим зашифровать и ключ
